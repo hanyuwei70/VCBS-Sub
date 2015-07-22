4aa37c4e8890bbd9d4afba740a2341c9aaf7b233
韩于惟 <hanyuwei70@qq.com>
2015-07-22 23:34:09 +0800

    tiny changes

--- src/models/show.php
+++ src/models/show.php
@@ -1,32 +1,32 @@
 <?php
 class show_Model
 {
-	public function __construct()
-	{
-	
-	}
 	private $subs=array
 	(
 		array(
-			'id'=1,
+			'id'=>1,
 			'title'=>'Test Title 1',
 			'content'=>'Test Content 1',
 			'download'=>1
 		),
 		array(
-			'id'=2,
+			'id'=>2,
 			'title'=>'Test Title2',
 			'content'=>'Test Content 2',
 			'download'=>2
 		)
-	)
-	public function get_article_by_id($id)
+	);
+	public function __construct()
+	{
+	
+	}
+	public function get_sub_by_id($id)
 	{
 		if(!is_int($id)) return null;
-		foreach ($subs as $nowsub)
+		foreach ($this->subs as $nowsub)
 		{
 			if ($nowsub['id']===$id)
-				return nowsub['id'];
+				return $nowsub;
 		}
 	}
 }
