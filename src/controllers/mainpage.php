1743935004f4d9c81db0a2962ab70504a4cb2ea9
icyfox <liuxuesi@bupt.edu.cn>
2015-09-24 22:23:21 +0800

    正常更新

--- src/controllers/mainpage.php
+++ src/controllers/mainpage.php
@@ -5,7 +5,10 @@ class Mainpage_Controller extends Base_Controller
 	{
 		$view=new Mainpage_View();
 		$view->loadtpl('./tpl/mainpage.tpl');
-
+		if (isset($_SESSION)) //用户已登录
+		{
+			
+		}
 	}
 }
 ?>
--- src/views/mainpage.php
+++ src/views/mainpage.php
@@ -1,13 +1,37 @@
 <?php
-class Mainpage_View
+/*
+Mainpage
+*/
+class Mainpage_View extends Base_View
 {
 	function __construct()
 	{
 	}
+	/*
+	 * loadtpl
+	 * 指定模板文件路径
+	 * @param $path $string 模板文件路径
+	 */
+	public function loadtpl($path)
+	{
+	}
+	/*
+	setparm
+	设定输出参数
+	@param $name $string 变量名
+	@param $val N/A 值
+	@return $int 执行结果
+	*/
+	public function setparm($name,$val)
+	{
+	}
+	/*
+	render
+	执行页面渲染输出
+	
+	*/
 	public function render()
 	{
-		$PAGE_TITLE=$this->model->PAGE_TITLE;
-		require_once('tpls/mainpage.tpl');
 	}
 	
 }
