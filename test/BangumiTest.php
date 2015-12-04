0ba5ea214f5a3b36864735072e80b4645acc9061
ParadiseDS <paradiseds888@gmail.com>
2015-12-04 21:31:39 +0800

    BangumiTest 初步已完成

--- test/BangumiTest.php
+++ test/BangumiTest.php
@@ -13,15 +13,14 @@ class Bangumi_ModelTest extends ModelTest
         $queryCount = $this->getConnection()->getRowCount('sub_bangumis');
         $id = $bangumi->create(2, 'test description');
         $queryCountAfter = $this->getConnection()->getRowCount('sub_bangumis');
-        $this->assertEquals(1, $queryCountAfter-$queryCount);
+        $this->assertEquals(1, $queryCountAfter-$queryCount, 'Failed to create new record:');
         $query = $this->getConnection()->createQueryTable('sub_bangumis', 'SELECT * FROM sub_bangumis ORDER BY id DESC LIMIT 1');
         $expect = new DbUnit_ArrayDataSet(array(
                                             'sub_bangumis' => array(
                                                 array('id' => $id, 'creator' => 2, 'createtime' => TIMENOW, 'owner' => 2, 'description' => 'test description'),
                                             )
                                           ));
-        $this->assertTablesEqual($expect->getTable('sub_bangumis'), $query);
-        return $id;
+        $this->assertTablesEqual($expect->getTable('sub_bangumis'), $query, 'Content after create do not match expection:');
     }
 
     public function testdel()
@@ -30,7 +29,7 @@ class Bangumi_ModelTest extends ModelTest
         $queryCount = $this->getConnection()->getRowCount('sub_bangumis');
         $rtn = $bangumi->del(2);
         $queryCountAfter = $this->getConnection()->getRowCount('sub_bangumis');
-        $this->assertEquals(-1, $queryCountAfter-$queryCount);
+        $this->assertEquals(-1, $queryCountAfter-$queryCount, 'Failed to del a exist record:');
         $query = $this->getConnection()->createQueryTable('sub_bangumis', 'SELECT id FROM sub_bangumis ORDER BY id');
         $expect = new DbUnit_ArrayDataSet(array(
                                             'sub_bangumis' => array(
@@ -46,30 +45,53 @@ class Bangumi_ModelTest extends ModelTest
         $bangumi = new Bangumi_Model();
         $query = $bangumi->getbanguminame(1);
         $expect = array('eng' => array('Fate Zero', 'Fate／Zero'), 'jpn' => array('フェイト/ゼロ'));
-        $this->assertEquals($expect, $query);
+        $this->assertEquals($expect, $query, 'Bangumi name result error:');
     }
 
     public function testvalidid()
     {
         $bangumi = new Bangumi_Model();
-        $valid = $bangumi->validid(2);
-        $this->assertEquals(Bangumi_Model::BANGUMI_VALID, $valid);
+        $result = $bangumi->validid(2);
+        $this->assertEquals(Bangumi_Model::BANGUMI_VALID, $result, 'Returned value error:');
         $this->setExpectedException('BangumiNotFound');
-        $valid = $bangumi->validid(0);
+        $result = $bangumi->validid(0);
     }
     /**
      * Test for Add name method
-     * @depends testcreate
+     * @depends testgetbanguminame
      */
-    // public function testaddname($id)
-    // {
-    //     $bangumi->addname($id, 'Fate Zero', 'eng');
-    //     $query = $this->getConnection()->createQueryTable('sub_bangumis_name', 'SELECT bangumi_id, name, lang FROM sub_bangumis_name');
-    //     $expect = new DbUnit_ArrayDataSet(array(
-    //                                             'sub_bangumis_name' => array(
-    //                                                 array('bangumi_id' => $id, 'name' => 'Fate Zero', 'lang' => 'eng'),
-    //                                             )
-    //                                         ));
-    //     $this->assertTablesEqual($query, $expect);
-    // }
+    public function testaddname()
+    {
+        $bangumi = new Bangumi_Model();
+        $result = $bangumi->addname(2, '数码宝贝', 'chs');
+        $this->assertEquals(Bangumi_Model::ADDNAME_SUCCESS, $result, 'Returned value error:');
+        $name_arr = $bangumi->getbanguminame(2);
+        $expect = array('chs' => array('数码兽大冒险', '数码宝贝'),
+                        'eng' => array('DIGIMON ADVENTURE'),
+                        'jpn' => array('デジモンアドベンチャー'),
+                        );
+        $this->assertEquals($expect, $name_arr, 'Content after add do not match expection:');
+        $result = $bangumi->addname(1, 'Fate Zero', 'eng');
+        $this->assertEquals(Bangumi_Model::ADDNAME_DUPE, $result, 'Returned value error when add dupe name:');
+    }
+    /**
+     * Test for Del name method
+     * @depends testgetbanguminame
+     */
+    public function testdelname()
+    {
+        $bangumi = new Bangumi_Model();
+        $result = $bangumi->delname(2, '数码兽大冒险');
+        $this->assertEquals(Bangumi_Model::DELNAME_SUCCESS, $result, 'Returned value error:');
+        $name_err = $bangumi->getbanguminame(2);
+        $expect = array('eng' => array('DIGIMON ADVENTURE'),
+                        'jpn' => array('デジモンアドベンチャー'),
+                        );
+        $this->assertEquals($expect, $name_err, 'Content after del do not match expection:');
+        $result = $bangumi->delname(2, '数码兽大冒险');
+        $this->assertEquals(Bangumi_Model::DELNAME_NOT_EXIST, $result, 'Returned value error when del nonexist name:');
+        $result = $bangumi->delname(2, 'DIGIMON ADVENTURE');
+        $result = $bangumi->delname(2, 'デジモンアドベンチャー');
+        $this->assertEquals(Bangumi_Model::DELNAME_LAST, $result, 'Returned value error when del last name:');
+    }
 }
